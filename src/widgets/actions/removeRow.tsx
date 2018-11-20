73ba16c8a6d95c716a76ceddce1415411f0fba43
Clayton Grassick <clayton@claytronics.org>
2018-11-20 09:17:58 -0500

    Add remove row

--- src/widgets/actions/removeRow.tsx
+++ src/widgets/actions/removeRow.tsx
@@ -1,27 +1,23 @@
 import * as React from 'react';
 import * as _ from 'lodash'
 import { ActionDef, Action, PerformActionOptions, RenderActionEditorProps, ValidateActionOptions } from '../actions';
-import { ExprValidator, LiteralExpr, Expr, ExprUtils } from 'mwater-expressions';
-import { ContextVar, createExprVariables } from '../blocks';
-import { LabeledProperty, PropertyEditor, TableSelect, ContextVarPropertyEditor } from '../propertyEditors';
-import { ContextVarExpr, ColumnValuesEditor } from '../columnValues';
-import { ExprComponent } from 'mwater-expressions-ui';
+import { LabeledProperty, PropertyEditor, ContextVarPropertyEditor } from '../propertyEditors';
 
 export interface RemoveRowActionDef extends ActionDef {
   type: "removeRow"
 
-  /** Context variable (row) to use for expression */
+  /** Context variable (row) to remove */
   contextVarId: string | null
-
-  /** Expression that generates id of row to be deleted */
-  idExpr: Expr
 }
 
+/** Remove a single row specified by a context variable */
 export class RemoveRowAction extends Action<RemoveRowActionDef> {
   async performAction(options: PerformActionOptions): Promise<void> {
+    const contextVar = options.contextVars.find(cv => cv.id === this.actionDef.contextVarId)
+
     // Remove row
-    const table = new ExprUtils(options.schema, createExprVariables(options.contextVars)).getExprIdTable(this.actionDef.idExpr)
-    const id = options.getContextVarExprValue(this.actionDef.contextVarId!, this.actionDef.idExpr)
+    const table = contextVar!.table!
+    const id = options.contextVarValues[this.actionDef.contextVarId!]
 
     // Do nothing if no row
     if (!id) {
@@ -29,7 +25,7 @@ export class RemoveRowAction extends Action<RemoveRowActionDef> {
     }
 
     const txn = options.database.transaction()
-    await txn.removeRow(table!, id)
+    await txn.removeRow(table, id)
     await txn.commit()
   }
 
@@ -40,56 +36,18 @@ export class RemoveRowAction extends Action<RemoveRowActionDef> {
       return "Context variable required"
     }
 
-    const exprValidator = new ExprValidator(options.schema, createExprVariables(options.contextVars))
-    let error: string | null
-    
-    // Validate expr
-    if (!this.actionDef.idExpr) {
-      return "Missing id expression"
-    }
-
-    error = exprValidator.validateExpr(this.actionDef.idExpr, { table: contextVar.table, types: ["id"] })
-    if (error) {
-      return error
-    }
-
     return null
   }
 
-  /** Get any context variables expressions that this action needs */
-  getContextVarExprs(contextVar: ContextVar) {
-    return (contextVar.id === this.actionDef.contextVarId && this.actionDef.idExpr) ? [this.actionDef.idExpr] : [] 
-  }
-  
   renderEditor(props: RenderActionEditorProps) {
-    const contextVar = props.contextVars.find(cv => cv.id === this.actionDef.contextVarId)
  
     return (
       <div>
-        <LabeledProperty label="Row Variable">
+        <LabeledProperty label="Row Variable to delete">
           <PropertyEditor obj={this.actionDef} onChange={props.onChange} property="contextVarId">
             {(value, onChange) => <ContextVarPropertyEditor value={value} onChange={onChange} contextVars={props.contextVars} types={["row"]} />}
           </PropertyEditor>
         </LabeledProperty>
-
-        { contextVar && contextVar.table 
-          ?
-          <LabeledProperty label="Id Expression">
-            <PropertyEditor obj={this.actionDef} onChange={props.onChange} property="idExpr">
-              {(value, onChange) => 
-                <ExprComponent 
-                  value={value} 
-                  onChange={onChange} 
-                  schema={props.schema} 
-                  dataSource={props.dataSource} 
-                  aggrStatuses={["individual", "literal"]}
-                  variables={createExprVariables(props.contextVars)}
-                  types={["id"]}
-                  table={contextVar.table!}/>
-              }
-              </PropertyEditor>
-            </LabeledProperty>
-          : null }
       </div>
     )            
   }
