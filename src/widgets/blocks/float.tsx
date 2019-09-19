f99661cc22bd184cdc396e29d9833c35238749f1
Clayton Grassick <clayton@claytronics.org>
2019-09-19 13:08:23 -0400

    Added float. Fix TOC

---
+++ src/widgets/blocks/float.tsx
@@ -0,0 +1,127 @@
+import * as React from 'react';
+import * as _ from 'lodash';
+import { BlockDef, RenderDesignProps, RenderInstanceProps, RenderEditorProps, ContextVar, ChildBlock } from '../blocks'
+import { LabeledProperty, PropertyEditor } from '../propertyEditors';
+import { Toggle } from 'react-library/lib/bootstrap';
+import CompoundBlock from '../CompoundBlock';
+import produce from 'immer';
+
+/** Floats some content either right or left of main content */
+export interface FloatBlockDef extends BlockDef {
+  type: "float"
+
+  /** Which way to float the float content */
+  direction: "left" | "right"
+
+  /** Which way to vertically align */
+  verticalAlign: "top" | "middle" | "bottom" 
+
+  /** Main content of block */
+  mainContent: BlockDef | null
+
+  /** Floated content of block */
+  floatContent: BlockDef | null
+}
+
+export class FloatBlock extends CompoundBlock<FloatBlockDef> {
+  getChildren(contextVars: ContextVar[]): ChildBlock[] {
+    // Get for all cells
+    return _.compact([this.blockDef.mainContent, this.blockDef.floatContent]).map(bd => ({ blockDef: bd!, contextVars: contextVars }))
+  }
+
+  validate() { return null }
+
+  processChildren(action: (self: BlockDef | null) => BlockDef | null): BlockDef {
+    return produce(this.blockDef, (draft: FloatBlockDef) => {
+      draft.mainContent = action(this.blockDef.mainContent)
+      draft.floatContent = action(this.blockDef.floatContent)
+    })
+  }
+
+  renderDesign(props: RenderDesignProps) {
+    const handleSetMainContent = (blockDef: BlockDef) => {
+      props.store.alterBlock(this.id, produce((b: FloatBlockDef) => { 
+        b.mainContent = blockDef 
+      }), blockDef.id)
+    }
+    const handleSetFloatContent = (blockDef: BlockDef) => {
+      props.store.alterBlock(this.id, produce((b: FloatBlockDef) => { 
+        b.floatContent = blockDef 
+      }), blockDef.id)
+    }
+
+    const mainContentNode = props.renderChildBlock(props, this.blockDef.mainContent, handleSetMainContent)
+    const floatContentNode = props.renderChildBlock(props, this.blockDef.floatContent, handleSetFloatContent)
+
+    return <FloatComponent 
+      float={floatContentNode} 
+      main={mainContentNode} 
+      direction={this.blockDef.direction} 
+      verticalAlign={this.blockDef.verticalAlign} />
+  }
+
+  renderInstance(props: RenderInstanceProps): React.ReactElement<any> {
+    const mainContentNode = props.renderChildBlock(props, this.blockDef.mainContent)
+    const floatContentNode = props.renderChildBlock(props, this.blockDef.floatContent)
+
+    return <FloatComponent 
+      float={floatContentNode} 
+      main={mainContentNode} 
+      direction={this.blockDef.direction} 
+      verticalAlign={this.blockDef.verticalAlign} />
+  }
+
+  renderEditor(props: RenderEditorProps) {
+    return (
+      <div>
+        <LabeledProperty label="Direction">
+          <PropertyEditor obj={this.blockDef} onChange={props.onChange} property="direction">
+            {(value, onChange) => 
+              <Toggle 
+                value={value} 
+                onChange={onChange} 
+                options={[
+                  { value: "left", label: <i className="fa fa-align-left"/> },
+                  { value: "right", label: <i className="fa fa-align-right"/> }
+                ]} />
+            }
+          </PropertyEditor>
+        </LabeledProperty>
+        <LabeledProperty label="Vertical Alignment">
+          <PropertyEditor obj={this.blockDef} onChange={props.onChange} property="verticalAlign">
+            {(value, onChange) => 
+              <Toggle 
+                value={value} 
+                onChange={onChange} 
+                options={[
+                  { value: "top", label: "Top" },
+                  { value: "middle", label: "Middle" },
+                  { value: "bottom", label: "Bottom" }
+                ]} />
+            }
+          </PropertyEditor>
+        </LabeledProperty>
+     </div>
+    )
+  }
+}
+
+const FloatComponent = (props: { 
+  main: React.ReactNode 
+  float: React.ReactNode
+  direction: "left" | "right"
+  verticalAlign: "top" | "middle" | "bottom"
+ }) => {
+  return (
+    <table style={{width: "100%"}}>
+      <tbody>
+        <tr>
+          { props.direction == "left" ? <td style={{ verticalAlign: props.verticalAlign }}>{props.float}</td> : null }
+          <td style={{ width: "100%", verticalAlign: props.verticalAlign }}>{props.main}</td>
+          { props.direction == "right" ? <td style={{ verticalAlign: props.verticalAlign }}>{props.float}</td> : null }
+        </tr>
+      </tbody>      
+    </table>
+
+  )
+}
