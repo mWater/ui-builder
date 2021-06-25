fd47ae9388646ab10a4c4d4424952c4ef996a94f
Clayton Grassick <clayton@claytronics.org>
2021-06-25 09:32:15 -0400

    Fix remove padding

--- src/widgets/blocks/toc/SplitPane.tsx
+++ src/widgets/blocks/toc/SplitPane.tsx
@@ -3,7 +3,7 @@ import FillDownwardComponent from "react-library/lib/FillDownwardComponent"
 
 /** Pane that is split left right */
 const SplitPane = ({ left, right, removePadding, theme }: { left: ReactNode, right : ReactNode, removePadding: boolean, theme: "light" | "dark" }) => {
-  const className = removePadding ? `toc-split remove-padding ${theme}` : `toc-split ${theme}`
+  const className = removePadding ? `toc-split normal-page-contents-remove-padding ${theme}` : `toc-split ${theme}`
   return <FillDownwardComponent>
     <div className={className}>
       <div key="left" className="toc-split-left">
